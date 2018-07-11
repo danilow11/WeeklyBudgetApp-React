a86ee6e6bcd99e3689ffc7be30f17f07b8e5239b
Daniel Alvarez <dan11nsa@gmail.com>
2018-07-10 20:50:16 -0500

    add reactstrap (and bootstrap css)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Button } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Weekly Budget App
+        <h1>Weekly Budget App</h1>
+        <Button color="primary">Testing reactstrap</Button>
       </div>
     );
   }
